08581dd363b6359f541ea6cb1a7f74284594585e
Bradley Leneus <6243042@collegemv.qc.ca>
2025-12-01 14:58:07 -0500

    Cours du 1er décembre

--- src/controllers/v3/BookingController.ts
+++ src/controllers/v3/BookingController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { BookingService } from "../../services/v3/BookingService";
+import { homedir } from "os";
 
 export class BookingController {
   public async createBooking(req: Request, res: Response): Promise<Response> {
     const { hotelId, userId, dateStart, dateEnd, nbRooms } = req.body;
-
+    console.log(hotelId, userId, dateStart, dateEnd, nbRooms)
     try {
       const booking = await BookingService.createBooking(
         hotelId,
@@ -19,7 +20,7 @@ export class BookingController {
     }
   }
 
-  public async getBookingByUserId(
+  public async getBookingsByUserId(
     req: Request,
     res: Response
   ): Promise<Response> {
@@ -27,7 +28,7 @@ export class BookingController {
 
     try {
       // Si l'utilisateur a filtré par cityName, on retourne le cityCode
-      const code = await BookingService.getBookingByUserId(userId as string);
+      const code = await BookingService.getBookingsByUserId(userId as string);
       if (code) {
         return res.status(200).json({ cityCode: code });
       } else {
@@ -43,4 +44,23 @@ export class BookingController {
       });
     }
   }
+
+
+  public async deleteBookingByHotelId(req:Request,res:Response):Promise<void>{
+    const {hotelId} = req.query;
+
+    try {     
+        if(hotelId != undefined)
+        await BookingService.deleteBookingByHotelId(hotelId as string)
+        res.status(204).send(); 
+       }
+     catch (error:unknown) {
+       res.status(404).json({
+        message: "L'id de l'hôtel n'existe pas",
+        error: error instanceof Error ? error.message : "Erreur inconnue",
+      });
+    }
+    
+    }
 }
+
