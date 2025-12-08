9d62211c5030ad2562bc2b45464f9916b164e776
samuel <monkeygmaming222@gmail.com>
2025-12-07 20:18:21 -0500

    enlever jest pour render

--- src/tests/user.test.ts
+++
@@ -1,7 +0,0 @@
-
-import { UserService } from '../services/v1/user.service';
-
-test('should return all users', async () => {
-  const users = await UserService.getAllUsers();
-  expect(users.length).toBe(1);
-});
