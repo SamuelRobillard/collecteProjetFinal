39ea6700fc91f5faf7581f5fc450ce17946b159e
Bradley Leneus <6243042@collegemv.qc.ca>
2025-12-08 11:40:58 -0500

    Ajout d'un middleware pour les dates  de booking

---
+++ src/middleswares/v3/validateBookingDates.ts
@@ -0,0 +1,20 @@
+
+import { Request,Response,NextFunction } from "express";
+
+
+const dateRegex= /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/ ;
+
+export function validateBookingDates(req:Request,res:Response,next:NextFunction) {
+    const {dateStart,dateEnd} = req.body ||{};
+
+    if(typeof dateStart !== 'string' || !dateRegex.test(dateStart)){
+        return res.status(400).json({error:'Date de début invalide'})
+    }
+
+    if(typeof dateEnd != 'string' || !dateRegex.test(dateEnd)){
+        return res.status(400).json({error:"Date de fin invalide."})
+    }
+    
+    next();
+}
+
--- src/routes/v3/BookingRoute.ts
+++ src/routes/v3/BookingRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { BookingController } from "../../controllers/v3/BookingController";
+import { validateBookingDates } from "../../middleswares/v3/validateBookingDates";
 
 const router = Router();
 const bookingController = new BookingController();
 
 router.get("/booking", bookingController.getBookingsByUserId);
-router.post("/booking", bookingController.createBooking);
+router.post("/booking",validateBookingDates, bookingController.createBooking);
 router.delete("/booking",bookingController.deleteBookingByHotelId);
 
 export default router;
