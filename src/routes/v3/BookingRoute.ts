92d14cae4df9a0340d965370ee4de9f4558137d8
Alexandre Rusenov <2391650@collegemv.qc.ca>
2025-12-08 11:10:16 -0500

    securisation des routes

--- src/routes/v3/BookingRoute.ts
+++ src/routes/v3/BookingRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { BookingController } from "../../controllers/v3/BookingController";
+import { authMiddleware } from "../../middleswares/authentificationMiddleswares";
 
 const router = Router();
 const bookingController = new BookingController();
 
 router.get("/booking", bookingController.getBookingsByUserId);
-router.post("/booking", bookingController.createBooking);
-router.delete("/booking",bookingController.deleteBookingByHotelId);
+router.post("/booking", authMiddleware, bookingController.createBooking);
+router.delete("/booking", authMiddleware, bookingController.deleteBookingByHotelId);
 
 export default router;
